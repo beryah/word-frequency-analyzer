f08c9b61d451b52ee39967e5e13b09e8b318ff6a
Todd Wang <sgm4320@gmail.com>
2017-07-29 12:15:04 +0800

    Done

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var fs = require('fs');
+var os = require('os');
+
+fs.readFile('input.txt', (err, buffer) => {
+
+    var result = {};
+
+    buffer.toString().split(/\r?\n/).forEach(word => {
+
+        if (result[word] === undefined) {
+            result[word] = 1;
+        }
+        else {
+            result[word]++;
+        }
+    });
+
+    var output = Object.keys(result).map(key => `<${key}> occurred ${result[key]} times`).join(os.EOL);
+
+    fs.writeFile('output.txt', output, () => console.log('Finished!'));
+});
+
